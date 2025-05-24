a2519bcd30864333cf8ead9476291e9c052a58a6
Arriaga2 <163058678+Arriaga2@users.noreply.github.com>
2025-05-24 15:08:23 -0600

    ejercicio a

---
+++ practica1/a.js
@@ -0,0 +1,6 @@
+let nombre = "Armando"; //  let porque el valor cambiará
+const edad = 25;         // const porque la edad no cambia
+nombre = "Ana Maria";
+
+const saludo = `Hola, ${nombre}. Tienes ${edad} años.`; // saludo no cambiará
+console.log(saludo); // muestra el saludo en consola
