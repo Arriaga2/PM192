6e3714d395917172ce44b8fa325cec0e10cf72b6
Arriaga2 <163058678+Arriaga2@users.noreply.github.com>
2025-06-11 18:51:33 -0600

    Practica 5

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,11 +1,13 @@
 /*Zona 1: importaciones */
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
 
-const Texto=(props)=>{
-  const {children}=props
+const Texto=()=>{
+  const [contenido, setContenido]=useState('Hola mundo React')
+  const actualizarTexto=()=>{setContenido('Estado Actualizado')}
   return(
-    <Text> {children} </Text>
+    <Text onPress={actualizarTexto}>{contenido}</Text>
   )
 }
 
@@ -13,9 +15,9 @@ const Texto=(props)=>{
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto> "Hola" </Texto>
-      <Texto> "Mundo" </Texto>
-      <Texto> "React Native" </Texto>
+      <Texto> </Texto>
+      <Texto> </Texto>
+      <Texto> </Texto>
       <Button title="Presioname!"> </Button>
       <StatusBar style="auto" />
     </View>
